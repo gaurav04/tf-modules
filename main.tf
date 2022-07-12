2fb8f152ee60248c54f20514db2c67ec3c18866d
gaurav04 <harsola.gaurav88@gmail.com>
2022-07-12 12:45:08 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,41 @@
+resource "aws_elasticache_subnet_group" "redis_cluster_subnet" {
+  name = "redis-test-subnet"
+  subnet_ids = ["subnet-2e986c45","subnet-bdf1abf1","subnet-e89ff393"]
+}
+
+resource "aws_elasticache_parameter_group" "redis_cluster_parameters" {
+  family = "redis3.2"
+  name   = "redis-test-params"  
+  parameter {
+    name  = "cluster-enabled"
+    value = "yes"
+  }
+}
+
+/*
+resource "aws_elasticache_parameter_group" "redis_cluster_parameters_six" {
+  family = "redis6.x"
+  name   = "redis-test-params-six"  
+  parameter {
+    name  = "cluster-enabled"
+    value = "yes"
+  }
+}
+*/
+
+resource "aws_elasticache_replication_group" "RedisCluster" {
+  node_type                     = "cache.t2.micro"
+  port                          = "6379"
+  engine_version                = "3.2.6"
+  replication_group_description = "Test"
+  replication_group_id          = "redis-test-rn"
+  parameter_group_name          = "${aws_elasticache_parameter_group.redis_cluster_parameters.name}"
+  security_group_ids            = ["sg-5cb87e20"]
+  automatic_failover_enabled    = true
+  cluster_mode {
+    num_node_groups         = 1
+    replicas_per_node_group = 0
+  }
+  subnet_group_name = "${aws_elasticache_subnet_group.redis_cluster_subnet.name}"
+
+}
