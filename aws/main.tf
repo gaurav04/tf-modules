e1e144cad18df385c7aca0a0d4a39246b3793903
gaurav04 <harsola.gaurav88@gmail.com>
2022-07-13 10:48:26 +0530

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -1,37 +1,37 @@
+locals {
+  family = {
+    "6.2" = "redis6.x"
+    "3.2.6" = "redis3.2"
+  }
+}
+
+variable "engine_version" {
+  default = "3.2.6"
+}
+
 resource "aws_elasticache_subnet_group" "redis_cluster_subnet" {
   name = "redis-test-subnet"
   subnet_ids = ["subnet-2e986c45","subnet-bdf1abf1","subnet-e89ff393"]
 }
 
 resource "aws_elasticache_parameter_group" "redis_cluster_parameters" {
-  family = "redis3.2"
-  name   = "redis-test-params"  
+  family = "${lookup(local.family, var.engine_version)}"
+  name   = "${var.engine_version == 6.2 ? "redis-rcl-params-vsix" : "redis-rcl-params"}"
+
   parameter {
     name  = "cluster-enabled"
     value = "yes"
   }
-}
 
-/*
-resource "aws_elasticache_parameter_group" "redis_cluster_parameters_vsix" {
-  family = "redis6.x"
-  name   = "redis-test-params-vsix"  
-  parameter {
-    name  = "cluster-enabled"
-    value = "yes"
+  lifecycle {
+    create_before_destroy = true
   }
 }
-*/
-
-variable "engine_version" {
-  default = "3.2.6"
-}
-
 
 resource "aws_elasticache_replication_group" "RedisCluster" {
   node_type                     = "cache.t2.micro"
   port                          = "6379"
-  engine_version                = "${var.engine_version == "6.2.6" ? "6.2.6" : "3.2.6"}"
+  engine_version                = "${var.engine_version}"
   replication_group_description = "Test"
   replication_group_id          = "redis-test-rn"
   parameter_group_name          = "${aws_elasticache_parameter_group.redis_cluster_parameters.name}"
@@ -42,4 +42,5 @@ resource "aws_elasticache_replication_group" "RedisCluster" {
     replicas_per_node_group = 0
   }
   subnet_group_name = "${aws_elasticache_subnet_group.redis_cluster_subnet.name}"
+  apply_immediately = true
 }
